2b6776c14a71f014b3c0066043e7748df9df088a
Konstantin <05kostman05@gmail.com>
2020-12-10 19:44:32 +0300

    my

---
+++ src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li v-for="(todo, index) in todos" v-on:click="deleteTodo(index)" v-bind:key="index">{{ todo }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['todos'],
+    methods: {
+        deleteTodo(index){
+            this.$emit('delete-todo', index);
+        }
+    }
+}
+</script>
+<style>li{width: 200px; border: solid; font-family: cursive; text-decoration: none; text-align: center; list-style: none; margin: auto; margin-top: 15px;}</style> 
